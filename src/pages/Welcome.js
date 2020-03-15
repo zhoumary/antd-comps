7203d6daddef49ba8cb9ba3ccde0fc2ba174692a
I323799 <fiona.zhou01@sap.com>
2020-03-15 22:21:48 +0800

    test branch

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
-import { Layout, Breadcrumb } from 'antd';
+import { Divider } from 'antd';
 
 import './Welcome.css';
 import MenuLayout from '../layouts/Menu';
